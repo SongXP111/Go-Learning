3115a7f46774628e8960d8819edd793c6946acf0
xipeng <sxp0728@126.com>
2024-09-07 21:41:15 -0400

    p38

--- demo3/interfaceDemo2.go
+++ demo3/interfaceDemo2.go
@@ -30,6 +30,7 @@ func interfaceDemo2() {
 	a = flag // 让bool类型实现这个接口
 	fmt.Printf("value: %v, type: %T\n", a, a) */
 
+	// 可以随意赋值
 	/* var a interface{}
 	a = 20
 	fmt.Printf("value: %v, type: %T\n", a, a)
@@ -38,9 +39,21 @@ func interfaceDemo2() {
 	a = true
 	fmt.Printf("value: %v, type: %T\n", a, a) */
 
-	show(20)
+	// 作为方法的参数
+	/* show(20)
 	show("Hello Golang")
 	show(false)
 	slice := []int{1, 2, 3, 4}
-	show(slice)
+	show(slice) */
+
+	// 可以作为map的value的类型
+	var m1 = make(map[string]interface{})
+	m1["username"] = "张三"
+	m1["age"] = 20
+	m1["married"] = false
+	fmt.Println(m1)
+
+	// 也可以放在切片里
+	var s1 = []interface{}{20, "hello Golang", true}
+	fmt.Println(s1)
 }
