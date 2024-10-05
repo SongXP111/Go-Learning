7e1f75940b657412e5dd3c88c07e44cf592c3d85
xipeng <sxp0728@126.com>
2024-10-04 22:10:39 -0400

    p54

--- demo8GoGin/gin3/main.go
+++ demo8GoGin/gin3/main.go
@@ -22,6 +22,21 @@ func main() {
 			"msg":   "我是msg",
 			"score": 91,
 			"hobby": []string{"吃饭", "睡觉", "玩游戏", "写代码"},
+			"newsList": []interface{}{
+				&Article{
+					Title:   "新闻标题111",
+					Content: "新闻内容111",
+				},
+				&Article{
+					Title:   "新闻标题222",
+					Content: "新闻内容222",
+				},
+			},
+			"testSlice": []string{},
+			"news": &Article{
+				Title:   "新闻标题",
+				Content: "新闻内容",
+			},
 		})
 	})
 	r.GET("/news", func(c *gin.Context) {
